aada7bf3bf0900203b45b19f1f27a7d52f147116
Edwin Baby <edwinbaby305@gmail.com>
2025-08-21 11:58:15 +0530

    removed the ewranings

--- src/components/silder/DesktopSlider.js
+++ src/components/silder/DesktopSlider.js
@@ -1,27 +1,25 @@
-import React, { useEffect, useRef, useState } from 'react'
-import './silder.css'
-export const DesktopSlider = () => {
-    const [images,setImages]=useState([])
-    const setInterval=useRef(null)
-    const selectDiv=useRef(null)
+// import React, { useEffect, useRef, useState } from 'react'
+// import './silder.css'
+// export const DesktopSlider = () => {
+//     const [images,setImages]=useState([])
+//     const setInterval=useRef(null)
+//     const selectDiv=useRef(null)
     
-    useEffect(()=>{
-       fetch('/data/product.json')
-       .then((response)=>{
-        return response.json()
-       })
-       .then((images)=>{
-        return setImages(images.silderDesktop)
-       })
-    },[])
-    console.log(images)
-  return (
-    <div className='des-silder-wrap'>
-       <div className='des-silder-inner'>
+//     useEffect(()=>{
+//        fetch('/data/product.json')
+//        .then((response)=>{
+//         return response.json()
+//        })
+//        .then((images)=>{
+//         return setImages(images.silderDesktop)
+//        })
+//     },[])
+//     console.log(images)
+//   return (
+//     <div className='des-silder-wrap'>
+//        <div className='des-silder-inner'>
           
-       </div>
-    </div>
-  )
-}
-
-
+//        </div>
+//     </div>
+//   )
+// }
